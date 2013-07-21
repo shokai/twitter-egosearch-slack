02e0e97aeeaa36a83b246b9cc47ff6b935fd356f
Sho Hashimoto <hashimoto@shokai.org>
2013-07-21 21:47:45 +0900

    config chat by topic name

--- libs/skype.rb
+++ libs/skype.rb
@@ -1,6 +1,12 @@
 module EgoSearch
   class Skype
-    @@chat = ::Skype.chats.find{|c| c.id == Config.skype.chat }
+    if Config.skype.kind_of? Hash and Config.skype.has_key? "topic"
+      @@chat = ::Skype.chats.find{|c| c.topic == Config.skype.topic }
+    elsif Config.skype.kind_of? Hash and Config.skype.has_key? "id"
+      @@chat = ::Skype.chats.find{|c| c.id == Config.skype.id }
+    else
+      raise StandardError, 'skype chat config error, please check "config.yml"'
+    end
 
     def self.post(message)
       @@chat.post message
